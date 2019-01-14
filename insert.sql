d3231b3327f7c63d654585f47815eb50652efb57
DruzhininDV <sinkrotro@mail.ru>
2019-01-14 22:12:58 +0300

    add DB

--- insert.sql
+++ insert.sql
@@ -1,60 +1,23 @@
+insert into calc (id_calc, name)
+values (4, 'Ночной (без лимита)');
 
-insert into calc_log (TIME, id_calc, status, name, startdate, enddate, calcdate)
-values (16, 4, 1, 'Ночной (без лимита)',('05-05-2014 18:34:01'),('05-05-2014 18:50:00'), ('05-05-2014 18:34:01'));
+insert into calc (id_calc, name)
+values (6, 'Ночной (по лимиту)');
 
-insert into calc_log (TIME, id_calc, status, name, startdate, enddate, calcdate)
-values (12, 6, 1, 'Ночной (по лимиту)',('05-05-2014 18:50:00'),('05-05-2014 19:02:05'), ('05-05-2014 18:50:00'));
+insert into calc (id_calc, name)
+values (10, 'Цены от поставщиков');
 
-insert into calc_log (TIME, id_calc, status, name, startdate, enddate, calcdate)
-values (14, 4, 1, 'Ночной (без лимита)',('30-06-2014 19:01:01'),('30-06-2014 19:15:07'),('30-06-2014 19:01:01'));
+insert into calc (id_calc, name)
+values (23, 'Расчет для КХД');
 
-insert into calc_log (TIME, id_calc, status, name, startdate, enddate, calcdate)
-values (10, 6, 1, 'Ночной (по лимиту)',('30-06-2014 19:15:07'),('30-06-2014 19:25:10'), ('30-06-2014 19:15:07'));
+insert into calc (id_calc, name)
+values (25, 'Расчет мин.цены для алкоголя');
 
-insert into calc_log (TIME, id_calc, status, name, startdate, enddate, calcdate)
-values (12, 6, 1, 'Ночной (по лимиту)',('17-09-2014 18:10:50'),('17-09-2014 18:22:50'), ('17-09-2014 18:10:50'));
+insert into calc (id_calc, name)
+values (33, 'Актуальный ассортимент');
 
-insert into calc_log (TIME, id_calc, status, name, startdate, enddate, calcdate)
-values (10, 4, 1, 'Ночной (без лимита)',('17-09-2014 18:00:05'),('17-09-2014 18:10:50'), ('17-09-2014 18:00:05'));
+insert into calc (id_calc, name)
+values (47, 'Затраты на доставку');
 
-insert into calc_log (TIME, id_calc, status, name, startdate, enddate, calcdate)
-values (146, 10, 1, 'Цены от поставщиков',('17-10-2014 15:00:00'),('17-10-2014 17:26:18'), ('17-10-2014 15:00:00'));
-
-insert into calc_log (TIME, id_calc, status, name, startdate, enddate, calcdate)
-values (13, 4, 1, 'Ночной (без лимита)',('14-11-2014 18:30:01'),('14-11-2014 18:43:48'), ('14-11-2014 18:30:01'));
-
-insert into calc_log (TIME, id_calc, status, name, startdate, enddate, calcdate)
-values (20, 6, 1, 'Ночной (по лимиту)',('14-11-2014 18:43:48'),('14-11-2014 19:04:11'), ('14-11-2014 18:43:48'));
-
-insert into calc_log (TIME, id_calc, status, name, startdate, enddate, calcdate)
-values (193, 10, 1, 'Цены от поставщиков',('25-11-2014 15:00:00'),('25-11-2014 18:13:14'), ('25-11-2014 15:00:00'));
-
-insert into calc_log (TIME, id_calc, status, name, startdate, enddate, calcdate)
-values (120, 10, 1, 'Цены от поставщиков',('09-12-2014 15:00:00'),('09-12-2014 17:00:26'), ('09-12-2014 15:00:00'));
-
-insert into calc_log (TIME, id_calc, status, name, startdate, enddate, calcdate)
-values (15, 4, 1, 'Ночной (без лимита)',('17-12-2014 18:30:01'),('17-12-2014 18:45:46'), ('17-12-2014 18:30:01'));
-
-insert into calc_log (TIME, id_calc, status, name, startdate, enddate, calcdate)
-values (18, 6, 1, 'Ночной (по лимиту)',('17-12-2014 18:45:46'),('17-12-2014 19:04:22'), ('17-12-2014 18:45:46'));
-
-insert into calc_log (TIME, id_calc, status, name, startdate, enddate, calcdate)
-values (114, 23, 1, 'Расчет для КХД',('13-04-2015 05:55:00'),('13-04-2015 07:49:08'), ('13-04-2015 05:55:00'));
-
-insert into calc_log (TIME, id_calc, status, name, startdate, enddate, calcdate)
-values (105, 23, 1, 'Расчет для КХД',('21-04-2015 06:00:00'),('21-04-2015 07:45:44'), ('21-04-2015 06:00:00'));
-
-insert into calc_log (TIME, id_calc, status, name, startdate, enddate, calcdate)
-values (17, 4, 1, 'Ночной (без лимита)',('27-04-2015 18:30:01'),('27-04-2015 18:47:12'), ('27-04-2015 18:30:01'));
-
-insert into calc_log (TIME, id_calc, status, name, startdate, enddate, calcdate)
-values (22, 6, 1, 'Ночной (по лимиту)',('27-04-2015 18:47:12'),('27-04-2015 19:09:18'), ('27-04-2015 18:47:12'));
-
-insert into calc_log (TIME, id_calc, status, name, startdate, enddate, calcdate)
-values (95, 23, 1, 'Расчет для КХД',('11-05-2015 06:00:00'),('11-05-2015 07:35:33'), ('11-05-2015 06:00:00'));
-
-insert into calc_log (TIME, id_calc, status, name, startdate, enddate, calcdate)
-values (36, 10, 1, 'Цены от поставщиков',('28-07-2015 16:00:00'),('28-07-2015 16:36:28'), ('28-07-2015 16:00:00'));
-
-insert into calc_log (TIME, id_calc, status, name, startdate, enddate, calcdate)
-values (138, 23, 1, 'Расчет для КХД',('05-08-2015 06:00:00'),('05-08-2015 08:18:33'), ('05-08-2015 06:00:00'));
+insert into calc (id_calc, name)
+values (55, 'Ночной(без лимита на послезавтра)');
