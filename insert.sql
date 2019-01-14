86de0203cc97edb96c5e1f13fcc5a3e57b2ea23f
DruzhininDV <sinkrotro@mail.ru>
2019-01-14 20:39:23 +0300

    add logi, setting. minus reset pass

---
+++ insert.sql
@@ -0,0 +1,26 @@
+nsert into calc (ID_calc, NAME)
+values (4, 'Ночной (без лимита)');
+
+insert into UNI_PRICEON.tbl_calc_example3 (T_ID, T_NAME)
+values (6, 'Ночной (по лимиту)');
+
+insert into UNI_PRICEON.tbl_calc_example3 (T_ID, T_NAME)
+values (10, 'Цены от поставщиков');
+
+insert into UNI_PRICEON.tbl_calc_example3 (T_ID, T_NAME)
+values (23, 'Расчет для КХД');
+
+insert into UNI_PRICEON.tbl_calc_example3 (T_ID, T_NAME)
+values (25, 'Расчет мин.цены для алкоголя');
+
+insert into UNI_PRICEON.tbl_calc_example3 (T_ID, T_NAME)
+values (33, 'Актуальный ассортимент');
+
+insert into UNI_PRICEON.tbl_calc_example3 (T_ID, T_NAME)
+values (47, 'Затраты на доставку');
+
+insert into UNI_PRICEON.tbl_calc_example3 (T_ID, T_NAME)
+values (55, 'Ночной(без лимита на послезавтра)');
+
+
+
