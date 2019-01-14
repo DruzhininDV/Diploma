39ca3382b173e7bbac59770890fb3c9085e21836
root <romask17@ya.ru>
2019-01-14 21:46:51 +0300

    get data from db

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,26 +1,37 @@
 from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required
-
+import sqlite3 as sql
+import os.path
 from app.model import Log
 
 main = Blueprint('main', __name__)
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR + '/../../', "app.db")
 
 @main.route('/')
 @main.route('/index')
 @login_required
 def index():
-    comments = [{'id': 1, 'name': 'Расчет для систем хранения', 'prior': 3},
-                {'id': 2, 'name': 'Цены продажи', 'prior': '2'},
-                {'id': 3, 'name': 'Расчет входных цен', 'prior': '5'}
-                ]
-    return render_template('main/index.html', comments=comments)
+    #
+    con = sql.connect(db_path)
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute("select * from main.calc")
+    calcs = cur.fetchall()
+    con.close()
+    return render_template('main/index.html', calcs=calcs)
 
 
 @main.route('/log')
 @login_required
 def log():
-    logs = Log.query.all()
+    con = sql.connect(db_path)
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute("select * from main.calc_log")
+    logs = cur.fetchall()
+    con.close()
     return render_template('main/log.html', logs=logs)
 
 
